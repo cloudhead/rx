8018a3d5b8db53fddb4e4e24fe8e9bde44e38bf5
Alexis Sellier <self@cloudhead.io>
2020-05-17 15:33:14 +0200

    Test archive format

--- tests/main.rs
+++ tests/main.rs
@@ -120,6 +120,11 @@ fn layers() {
     test("layers");
 }
 
+#[test]
+fn archive() {
+    test("archive");
+}
+
 ////////////////////////////////////////////////////////////////////////////////
 
 fn test(name: &str) {
@@ -129,9 +134,10 @@ fn test(name: &str) {
 }
 
 fn run(name: &str) -> io::Result<()> {
-    // We allow tests to create this temporary file,
+    // We allow tests to create these temporary files,
     // so make sure it's not there when a test is run.
     fs::remove_file("/tmp/rx.png").ok();
+    fs::remove_file("/tmp/archive.rxa").ok();
 
     let path = Path::new(env!("CARGO_MANIFEST_DIR"))
         .join("tests")
